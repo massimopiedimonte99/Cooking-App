46eed6c357ab086797d88f148ebcdbebc7ed3bd7
Massimo Piedimonte <>
2022-03-15 20:54:18 +0100

    Aggiunto Angular Material, Bootstrap e FA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'CookingApp';
+
+  openModal() { 
+    console.log("LOL");
+  }
 }
